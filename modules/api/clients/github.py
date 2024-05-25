caf1bfcdff72a7202f043c30c5510ad39fbeb861
Olha Shaiakhmedova <rakkastantalvi@gmail.com>
2024-05-25 16:38:48 +0300

    Project task#4 - test API Github via pytest and requests/main part

---
+++ modules/api/clients/github.py
@@ -0,0 +1,16 @@
+import requests
+
+class GitHub:
+
+    def get_user(self, username):
+        r = requests.get(f'http://api.github.com/users/{username}')
+        body = r.json()
+        return body
+    
+    def search_repo(self, name):
+        r = requests.get("http://api.github.com/search/repositories",
+                         params={"q": name}
+        )
+        body = r.json()
+        return body
+    
